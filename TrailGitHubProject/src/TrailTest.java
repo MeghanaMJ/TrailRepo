a3febae730cd8d06f38221c695722b347ece1bcc
MeghanaMJ <45536551+MeghanaMJ@users.noreply.github.com>
2019-01-24 17:55:55 +0530

    Update TrailTest.java

--- TrailGitHubProject/src/TrailTest.java
+++ TrailGitHubProject/src/TrailTest.java
@@ -4,6 +4,7 @@ public class TrailTest {
 	public static void main(String[] args) {
 		System.out.println("Trial Test Execueted");
 		System.out.println("Second Trial");
+		System.out.println("ThIrD tRiAl");
 
 	}
 
